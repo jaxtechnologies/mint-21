34edc92f610b985831726ac705134ae994463a42
jaxtechnologies <104657792+jaxtechnologies@users.noreply.github.com>
2022-05-22 11:29:51 -0400

    Add files via upload
    Added initial mint setup script

---
+++ mint_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-get install openssh-server -y
+sudo apt-get install vim -y
+sudo apt-get install chromium-browser -y
+sudo apt-get install gtkhash -y
+sudo apt-get install remmina -y
+sudo apt-get install remmina-plugin-rdp -y
+sudo apt-get install remmina-plugin-vnc -y
+sudo apt-get install scribus -y
+sudo apt-get install scribus-template -y
+sudo apt-get install sublime-text -y
+sudo apt-get install dos2unix -y
+sudo apt-get install conky-all -y
