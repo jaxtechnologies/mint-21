8638c9707ec4a8860569284a78364a48206546fb
jaxtechnologies <104657792+jaxtechnologies@users.noreply.github.com>
2022-05-23 18:41:58 -0400

    Update mint_setup.sh
    Added 2.json file to setup taskbar icons after installs...

--- mint_setup.sh
+++ mint_setup.sh
@@ -41,3 +41,9 @@ sudo apt-get install handbrake -y
 sudo apt-get install virtualbox -y
 sudo apt-get install virtualbox-guest-additions-iso -y
 sudo apt-get install falkon -y
+
+wget -L https://raw.githubusercontent.com/jaxtechnologies/mint-setup/main/2.json
+
+mv 2.json .cinnamon/configs/grouped-window-list@cinnamon.org/
+
+sudo reboot
