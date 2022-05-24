d0d0e1b3617ac923a6dc5f85699808bdbcd14118
jaxtechnologies <104657792+jaxtechnologies@users.noreply.github.com>
2022-05-23 22:18:57 -0400

    Update mint_setup.sh
    Adding comments to Remina Install...

--- mint_setup.sh
+++ mint_setup.sh
@@ -7,7 +7,7 @@ sudo apt-get install openssh-server -y
 sudo apt-get install vim -y
 sudo apt-get install chromium-browser -y
 sudo apt-get install gtkhash -y
-sudo apt-get install remmina -y
+sudo apt-get install remmina -y    #use for SSH and RDP connections
 sudo apt-get install remmina-plugin-rdp -y
 sudo apt-get install remmina-plugin-vnc -y
 sudo apt-get install scribus -y
