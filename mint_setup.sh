e2c0789c48b8c2defa09a116ca1c31aafb1f238b
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2022-06-07 14:21:50 -0400

    Update mint_setup.sh
    Removed sublime-text as it requires a license and also removed netbeans...  Also modified install lines with a variable for the total number...

--- mint_setup.sh
+++ mint_setup.sh
@@ -7,37 +7,40 @@ sudo sed -i 's/http:\/\/archive.ubuntu.com\/ubuntu/http:\/\/mirror.clarkson.edu\
 sudo apt-get update
 sudo apt-get upgrade -y
 
+#TOTAL APPS BEING ADDED TO INSTALLATION
+total=38
+
 clear
 echo ""
-echo Installing 1/40 openssh-server...  Please be patient...
+echo Installing 1/$total openssh-server...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install openssh-server -y
 
 clear
 echo ""
-echo Installing 2/40 vim...  Please be patient...
+echo Installing 2/$total vim...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install vim -y
 
 clear
 echo ""
-echo Installing 3/40 chromium-browser...  Please be patient...
+echo Installing 3/$total chromium-browser...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install chromium-browser -y
 
 clear
 echo ""
-echo Installing 4/40 gtkhash...  Please be patient...
+echo Installing 4/$total gtkhash...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install gtkhash -y
 
 clear
 echo ""
-echo Installing 5/40 remmina...  Please be patient...
+echo Installing 5/$total remmina...  Please be patient...
 echo ""
 echo ""
 #use for SSH and RDP connections
@@ -45,71 +48,63 @@ sudo apt-get install remmina -y
 
 clear
 echo ""
-echo Installing 6/40 remmina-plugin-rdp...  Please be patient...
+echo Installing 6/$total remmina-plugin-rdp...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install remmina-plugin-rdp -y
 
 clear
 echo ""
-echo Installing 7/40 remmina-plugin-vnc...  Please be patient...
+echo Installing 7/$total remmina-plugin-vnc...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install remmina-plugin-vnc -y
 
 clear
 echo ""
-echo Installing 8/40 scribus...  Please be patient...
+echo Installing 8/$total scribus...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install scribus -y
 
 clear
 echo ""
-echo Installing 9/40 scribus-template...  Please be patient...
+echo Installing 9/$total scribus-template...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install scribus-template -y
 
 clear
 echo ""
-echo Installing 10/40 notepadqq...  Please be patient...
+echo Installing 10/$total notepadqq...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install notepadqq -y
 
 clear
 echo ""
-echo Installing 11/40 sublime-text...  Please be patient...
-echo ""
-echo ""
-#not sure this is free
-sudo apt-get install sublime-text -y
-
-clear
-echo ""
-echo Installing 12/40 dos2unix...  Please be patient...
+echo Installing 11/$total dos2unix...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install dos2unix -y
 
 clear
 echo ""
-echo Installing 13/40 keepass2...  Please be patient...
+echo Installing 12/$total keepass2...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install keepass2 -y
 
 clear
 echo ""
-echo Installing 14/40 conky-all...  Please be patient...
+echo Installing 13/$total conky-all...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install conky-all -y
 
 clear
 echo ""
-echo Installing 15/40 vnstat...  Please be patient...
+echo Installing 14/$total vnstat...  Please be patient...
 echo ""
 echo ""
 #required by conky network statistics section
@@ -117,7 +112,7 @@ sudo apt-get install vnstat -y
 
 clear
 echo ""
-echo Installing 16/40 psensor...  Please be patient...
+echo Installing 15/$total psensor...  Please be patient...
 echo ""
 echo ""
 #graphical sensor monitor
@@ -125,7 +120,7 @@ sudo apt-get install psensor -y
 
 clear
 echo ""
-echo Installing 17/40 tree...  Please be patient...
+echo Installing 16/$total tree...  Please be patient...
 echo ""
 echo ""
 #helps determine system monitors for conky
@@ -133,14 +128,14 @@ sudo apt-get install tree -y
 
 clear
 echo ""
-echo Installing 18/40 python-pip...  Please be patient...
+echo Installing 17/$total python-pip...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install python-pip -y
 
 clear
 echo ""
-echo Installing 19/40 python3-tk...  Please be patient...
+echo Installing 18/$total python3-tk...  Please be patient...
 echo ""
 echo ""
 #test ‘python3’ tkinkter app before install
@@ -148,7 +143,7 @@ sudo apt-get install python3-tk -y
 
 clear
 echo ""
-echo Installing 20/40 tk...  Please be patient...
+echo Installing 19/$total tk...  Please be patient...
 echo ""
 echo ""
 #test ‘python3’ tkinkter app before install
@@ -156,21 +151,21 @@ sudo apt-get install tk -y
 
 clear
 echo ""
-echo Installing 21/40 docker.io...  Please be patient...
+echo Installing 20/$total docker.io...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install docker.io -y
 
 clear
 echo ""
-echo Installing 22/40 sqlitebrowser...  Please be patient...
+echo Installing 21/$total sqlitebrowser...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install sqlitebrowser -y
 
 clear
 echo ""
-echo Installing 23/40 pdfshuffler...  Please be patient...
+echo Installing 22/$total pdfshuffler...  Please be patient...
 echo ""
 echo ""
 #Merge or Split PDF Documents
@@ -178,7 +173,7 @@ sudo apt-get install pdfshuffler -y
 
 clear
 echo ""
-echo Installing 24/40 okular...  Please be patient...
+echo Installing 23/$total okular...  Please be patient...
 echo ""
 echo ""
 #Document Reader - PDF’s, etc
@@ -186,35 +181,28 @@ sudo apt-get install okular -y
 
 clear
 echo ""
-echo Installing 25/40 kleopatra...  Please be patient...
+echo Installing 24/$total kleopatra...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install kleopatra -y
 
 clear
 echo ""
-echo Installing 26/40 synergy...  Please be patient...
+echo Installing 25/$total synergy...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install synergy -y
 
 clear
 echo ""
-echo Installing 27/40 mysql-workbench...  Please be patient...
+echo Installing 26/$total mysql-workbench...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install mysql-workbench -y
 
 clear
 echo ""
-echo Installing 28/40 netbeans...  Please be patient...
-echo ""
-echo ""
-sudo apt-get install netbeans -y
-
-clear
-echo ""
-echo Installing 29/40 steam...  Please be patient...
+echo Installing 27/$total steam...  Please be patient...
 echo ""
 echo ""
 #Steam Gaming
@@ -222,7 +210,7 @@ sudo apt-get install steam -y
 
 clear
 echo ""
-echo Installing 30/40 cmatrix...  Please be patient...
+echo Installing 28/$total cmatrix...  Please be patient...
 echo ""
 echo ""
 #cmatrix turns terminal into matrix terminal
@@ -230,7 +218,7 @@ sudo apt-get install cmatrix -y
 
 clear
 echo ""
-echo Installing 31/40 krita...  Please be patient...
+echo Installing 29/$total krita...  Please be patient...
 echo ""
 echo ""
 #Professional Paint Program
@@ -238,7 +226,7 @@ sudo apt-get install krita -y
 
 clear
 echo ""
-echo Installing 32/40 cheese...  Please be patient...
+echo Installing 30/$total cheese...  Please be patient...
 echo ""
 echo ""
 #Camera Software
@@ -246,7 +234,7 @@ sudo apt-get install cheese -y
 
 clear
 echo ""
-echo Installing 33/40 guvcview...  Please be patient...
+echo Installing 31/$total guvcview...  Please be patient...
 echo ""
 echo ""
 #Camera Software
@@ -254,42 +242,42 @@ sudo apt-get install guvcview -y
 
 clear
 echo ""
-echo Installing 34/40 gftp...  Please be patient...
+echo Installing 32/$total gftp...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install gftp -y
 
 clear
 echo ""
-echo Installing 35/40 handbrake...  Please be patient...
+echo Installing 33/$total handbrake...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install handbrake -y
 
 clear
 echo ""
-echo Installing 36/40 virtualbox...  Please be patient...
+echo Installing 34/$total virtualbox...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install virtualbox -y
 
 clear
 echo ""
-echo Installing 37/40 virtualbox-guest-additions-iso...  Please be patient...
+echo Installing 35/$total virtualbox-guest-additions-iso...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install virtualbox-guest-additions-iso -y
 
 clear
 echo ""
-echo Installing 38/40 falkon...  Please be patient...
+echo Installing 36/$total falkon...  Please be patient...
 echo ""
 echo ""
 sudo apt-get install falkon -y
 
 clear
 echo ""
-echo Installing 39/40 openoffice.org-hyphenation...  Please be patient...
+echo Installing 37/$total openoffice.org-hyphenation...  Please be patient...
 echo ""
 echo ""
 #Fixes the Local Language Packs message after Initial Installation
@@ -297,7 +285,7 @@ sudo apt-get install openoffice.org-hyphenation -y
 
 clear
 echo ""
-echo Installing 40/40 xrdp...  Please be patient...
+echo Installing 38/$total xrdp...  Please be patient...
 echo ""
 echo ""
 #Allows RDP Connections to Linux
