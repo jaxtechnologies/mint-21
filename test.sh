c9c068221b22f337e01c1c4a86cd23ee66760370
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2022-08-12 23:23:25 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,36 @@
+clear
+echo ""
+echo Installing 13/$total conky-all...  Please be patient...
+echo ""
+echo ""
+sudo apt-get install conky-all -y
+
+clear
+echo ""
+echo Installing 14/$total vnstat...  Please be patient...
+echo ""
+echo ""
+#required by conky network statistics section
+sudo apt-get install vnstat -y
+
+clear
+echo ""
+echo Installing 15/$total psensor...  Please be patient...
+echo ""
+echo ""
+#graphical sensor monitor
+sudo apt-get install psensor -y
+
+clear
+echo ""
+echo Installing 16/$total tree...  Please be patient...
+echo ""
+echo ""
+#helps determine system monitors for conky
+sudo apt-get install tree -y
+
+
+
 cd ~/Downloads/mint-setup/
 mv 2.json ~/.cinnamon/configs/grouped-window-list@cinnamon.org/
 mv linuxmint.png ~/Pictures/
