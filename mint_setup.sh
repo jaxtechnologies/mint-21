8b187f2cdde0f0f4ca1e78225851f4bc022b48ff
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2022-08-12 20:55:12 -0400

    Update mint_setup.sh

--- mint_setup.sh
+++ mint_setup.sh
@@ -338,6 +338,7 @@ mv 2.json ~/.cinnamon/configs/grouped-window-list@cinnamon.org/
 mv linuxmint.png ~/Pictures/
 mv .conkyrc ~
 mv StartupConky.desktop ~/.config/autostart/
+sudo mv sunset sunrise /usr/local/bin/
 
 sudo mv sun-hours_etc_cron.daily /etc/cron.daily/sun-hours
 sudo chown root.root /etc/cron.daily/sun-hours
@@ -347,7 +348,11 @@ sudo mv sun-hours_usr_local_bin /usr/local/bin/sun-hours
 sudo chown root.root /usr/local/bin/sun-hours
 sudo chmod 755 /usr/local/bin/sun-hours
 
+echo ""
+echo ""
+echo Getting current Sunset and Sunrise times...  Please be patient...
 sudo /usr/local/bin/sun-hours
+sleep 10
 
 #Testing to see if this step is needed since we are rebooting and if will fix sunrise-sunset issue not running...
 #sudo /etc/init.d/cron restart
