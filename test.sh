f4ec6b7bfe5795cb3b176b7e9fe029e2bc121a35
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2022-08-14 08:37:03 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,13 +6,13 @@ select opt in vm e6530 macpro quit; do
 
   case $opt in
     vm)
-      mv $install_path/.conkyrc_vm ~/.conkrc
+      mv $install_path/.conkyrc_vm ~/.conkyrc
       ;;
     e6530)
-      mv $install_path/.conkyrc_e6530 ~/.conkrc
+      mv $install_path/.conkyrc_e6530 ~/.conkyrc
       ;;
     macpro)
-      mv $install_path/.conkyrc_e6530 ~/.conkrc   
+      mv $install_path/.conkyrc_e6530 ~/.conkyrc   
       ;;
     quit)
       break
