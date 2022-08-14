a991d19e377e5a2ceeef820013a6d1ce4b6f8713
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2022-08-14 13:38:40 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,17 +7,20 @@ select opt in vm e6530 macpro quit; do
   case $opt in
     vm)
       mv $install_path/.conkyrc_vm ~/.conkyrc
+      break
       ;;
     e6530)
       mv $install_path/.conkyrc_e6530 ~/.conkyrc
+      break
       ;;
     macpro)
-      mv $install_path/.conkyrc_e6530 ~/.conkyrc   
+      mv $install_path/.conkyrc_e6530 ~/.conkyrc
+      break
       ;;
     quit)
       break
       ;;
-    *) 
+    *)
       echo "Invalid option $REPLY"
       ;;
   esac
