e0ca1688afee2e8150bfcdf56bc7c775260850d6
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2022-11-11 14:38:20 -0500

    Update mint_setup.sh

--- mint_setup.sh
+++ mint_setup.sh
@@ -1,7 +1,42 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
+# NAME: JTOS
+# DESC: An installation and deployment script for JaxTech's Mint Cinnamon desktop.
+# Adapted from Derek Taylor's (DistroTube) DTOS script...
+# Top logo inspiration came from Chris Titus's ArchTitus script...
 ### TODO
 ### Redshift Startup
+
+clear
+
+echo -ne "
+   ████████╗  █████╗  ██╗   ██╗  ████████╗ ███████╗  ██████╗ ██╗  ██╗
+   ╚══██╔══╝ ██╔══██╗  ██╗ ██╔╝  ╚══██╔══╝ ██╔════╝ ██╔════╝ ██║  ██║
+      ██║    ███████║   ████╔╝      ██║    ███████╗ ██║      ███████║
+   ██ ██║    ██╔══██║  ██╔╝██╗      ██║    ██╔════╝ ██║      ██╔══██║
+   ╚███╔╝    ██║  ██║ ██╔╝  ██╗     ██║    ███████╗ ╚██████╗ ██║  ██║
+    ╚══╝     ╚═╝  ╚═╝ ╚═╝   ╚═╝     ╚═╝    ╚══════╝  ╚═════╝ ╚═╝  ╚═╝
+"
+echo " JTOS script will launch in 5 seconds..."
+
+sleep 5
+
+
+clear
+echo ""
+read -p "Does this machine have plenty of drive space for timeshift setup...  Say NO for Virtual Machine setup... (y/n)? "
+if [ "$REPLY" = "y" ]; then
+	
+  echo ""
+  echo Setting up Timeshift and taking first snapshot...  Please be patient...
+  echo ""
+  echo ""
+  sudo timeshift --create --rsync --comment "Base Install Snapshot" --yes
+  sudo sed -i 's/"schedule_weekly" : "false"/"schedule_weekly" : "true"/g' /etc/timeshift/timeshift.json
+
+else
+	cancel
+fi
+
 
 install_path=$(pwd)
 
@@ -12,278 +47,44 @@ sudo sed -i 's/http:\/\/archive.ubuntu.com\/ubuntu/http:\/\/mirror.clarkson.edu\
 sudo apt update
 sudo apt upgrade -y
 
-#TOTAL APPS BEING ADDED TO INSTALLATION
-total=36
-
-clear
-echo ""
-echo Installing 1/$total openssh-server...  Please be patient...
-echo ""
-echo ""
 sudo apt install openssh-server -y
-
-clear
-echo ""
-echo Installing 2/$total vim...  Please be patient...
-echo ""
-echo ""
 sudo apt install vim -y
-
-clear
-echo ""
-echo Installing 3/$total dos2unix...  Please be patient...
-echo ""
-echo ""
 sudo apt install dos2unix -y
-
-clear
-echo ""
-echo Installing 4/$total gtkhash...  Please be patient...
-echo ""
-echo ""
 sudo apt install gtkhash -y
-
-clear
-echo ""
-echo Installing 5/$total remmina...  Please be patient...
-echo ""
-echo ""
-#use for SSH and RDP connections
-sudo apt install remmina -y
-
-clear
-echo ""
-echo Installing 6/$total notepadqq...  Please be patient...
-echo ""
-echo ""
+sudo apt install remmina -y		#use for SSH and RDP connections
 sudo apt install notepadqq -y
-
-clear
-echo ""
-echo Installing 7/$total keepass2...  Please be patient...
-echo ""
-echo ""
 sudo apt install keepass2 -y
-
-clear
-echo ""
-echo Installing 8/$total gftp...  Please be patient...
-echo ""
-echo ""
 sudo apt install gftp -y
-
-clear
-echo ""
-echo Installing 9/$total pdfarranger...  Please be patient...
-echo ""
-echo ""
-#Merge or Split PDF Documents
-sudo apt install pdfarranger -y
-
-clear
-echo ""
-echo Installing 10/$total okular...  Please be patient...
-echo ""
-echo ""
-#Document Reader - PDF’s, etc
-sudo apt install okular -y
-
-clear
-echo ""
-echo Installing 11/$total handbrake...  Please be patient...
-echo ""
-echo ""
+sudo apt install pdfarranger -y		#Merge or Split PDF Documents
+sudo apt install okular -y		#Document Reader - PDF’s, etc
 sudo apt install handbrake -y
-
-clear
-echo ""
-echo Installing 12/$total cheese...  Please be patient...
-echo ""
-echo ""
-#Camera Software
-sudo apt install cheese -y
-
-clear
-echo ""
-echo Installing 13/$total guvcview...  Please be patient...
-echo ""
-echo ""
-#Camera Software
-sudo apt install guvcview -y
-
-clear
-echo ""
-echo Installing 14/$total scribus...  Please be patient...
-echo ""
-echo ""
+sudo apt install cheese -y		#Camera Software
+sudo apt install guvcview -y		#Camera Software
 sudo apt install scribus -y
-
-clear
-echo ""
-echo Installing 15/$total scribus-template...  Please be patient...
-echo ""
-echo ""
 sudo apt install scribus-template -y
-
-clear
-echo ""
-echo Installing 16/$total krita...  Please be patient...
-echo ""
-echo ""
-#Professional Paint Program
-sudo apt install krita -y
-
-clear
-echo ""
-echo Installing 17/$total virtualbox...  Please be patient...
-echo ""
-echo ""
+sudo apt install krita -y		#Professional Paint Program
 sudo apt install virtualbox -y
-
-clear
-echo ""
-echo Installing 18/$total virtualbox-ext-pack...  Please be patient...
-echo ""
-echo ""
 sudo apt install virtualbox-ext-pack -y
-
-clear
-echo ""
-echo Installing 19/$total virtualbox-guest-additions-iso...  Please be patient...
-echo ""
-echo ""
 sudo apt install virtualbox-guest-additions-iso -y
-
-clear
-echo ""
-echo Installing 20/$total kleopatra...  Please be patient...
-echo ""
-echo ""
 sudo apt install kleopatra -y
-
-clear
-echo ""
-echo Installing 21/$total conky-all...  Please be patient...
-echo ""
-echo ""
 sudo apt install conky-all -y
-
-clear
-echo ""
-echo Installing 22/$total vnstat...  Please be patient...
-echo ""
-echo ""
-#required by conky network statistics section
-sudo apt install vnstat -y
-
-clear
-echo ""
-echo Installing 23/$total psensor...  Please be patient...
-echo ""
-echo ""
-#graphical sensor monitor
-sudo apt install psensor -y
-
-clear
-echo ""
-echo Installing 24/$total tree...  Please be patient...
-echo ""
-echo ""
-#helps determine system monitors for conky
-sudo apt install tree -y
-
-clear
-echo ""
-echo Installing 25/$total cmatrix...  Please be patient...
-echo ""
-echo ""
-#cmatrix turns terminal into matrix terminal
-sudo apt install cmatrix -y
-
-clear
-echo ""
-echo Installing 26/$total steam...  Please be patient...
-echo ""
-echo ""
-#Steam Gaming
-sudo apt install steam -y
-
-clear
-echo ""
-echo Installing 27/$total pingus...  Please be patient...
-echo ""
-echo ""
-#Pingus Game
-sudo apt install pingus -y
-
-clear
-echo ""
-echo Installing 28/$total frozen-bubble...  Please be patient...
-echo ""
-echo ""
-#Frozen Bubble Game
-sudo apt install frozen-bubble -y
-
-clear
-echo ""
-echo Installing 29/$total python3-pip...  Please be patient...
-echo ""
-echo ""
+sudo apt install vnstat -y		#required by conky network statistics section
+sudo apt install psensor -y		#graphical sensor monitor
+sudo apt install tree -y		#helps determine system monitors for conky
+sudo apt install cmatrix -y		#cmatrix turns terminal into matrix terminal
+sudo apt install steam -y		#Steam Gaming
+sudo apt install pingus -y		#Pingus Game
+sudo apt install frozen-bubble -y	#Frozen Bubble Game
 sudo apt install python3-pip -y
-
-clear
-echo ""
-echo Installing 30/$total python3-tk...  Please be patient...
-echo ""
-echo ""
-#test ‘python3’ tkinkter app before install
-sudo apt install python3-tk -y
-
-clear
-echo ""
-echo Installing 31/$total tk...  Please be patient...
-echo ""
-echo ""
-#test ‘python3’ tkinkter app before install
-sudo apt install tk -y
-
-clear
-echo ""
-echo Installing 32/$total docker.io...  Please be patient...
-echo ""
-echo ""
+sudo apt install python3-tk -y		#test ‘python3’ tkinkter app before install
+sudo apt install tk -y			#test ‘python3’ tkinkter app before install
 sudo apt install docker.io -y
-
-clear
-echo ""
-echo Installing 33/$total sqlitebrowser...  Please be patient...
-echo ""
-echo ""
 sudo apt install sqlitebrowser -y
+sudo apt install openoffice.org-hyphenation -y	#Fixes the Local Language Packs message after Initial Installation
+sudo apt install falkon -y		#Web browser
 
-clear
-echo ""
-echo Installing 34/$total openoffice.org-hyphenation...  Please be patient...
-echo ""
-echo ""
-#Fixes the Local Language Packs message after Initial Installation
-sudo apt install openoffice.org-hyphenation -y
-
-clear
-echo ""
-echo Installing 35/$total xrdp...  Please be patient...
-echo ""
-echo ""
-#Allows RDP Connections to Linux
-sudo apt install xrdp -y
+sudo apt install xrdp -y		#Allows RDP Connections to Linux
 sudo adduser xrdp ssl-cert
-
-clear
-echo ""
-echo Installing 36/$total falkon...  Please be patient...
-echo ""
-echo ""
-sudo apt install falkon -y
 
 
 clear
@@ -418,23 +219,6 @@ if [ "$REPLY" = "y" ]; then
   echo ""
   echo ""
   sudo apt install sublime-text -y
-
-else
-	cancel
-fi
-
-
-clear
-echo ""
-read -p "Does this machine have plenty of drive space for timeshift setup...  Say NO for Virtual Machine setup... (y/n)? "
-if [ "$REPLY" = "y" ]; then
-	
-  echo ""
-  echo Setting up Timeshift and taking first snapshot...  Please be patient...
-  echo ""
-  echo ""
-  sudo timeshift --create --rsync --comment "Base Install Snapshot" --yes
-  sudo sed -i 's/"schedule_weekly" : "false"/"schedule_weekly" : "true"/g' /etc/timeshift/timeshift.json
 
 else
 	cancel
